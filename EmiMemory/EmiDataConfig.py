edba7f086b613a05afef482295950dbbe161f407
JDavid <davidanayaacosta@gmail.com>
2021-09-21 21:33:20 -0500

    Emi Beta .1.1

---
+++ EmiMemory/EmiDataConfig.py
@@ -0,0 +1,6 @@
+import mysql.connector as SConection
+
+baseConect = SConection.connect(host="localhost", user="root", passwd="", database="emidata")
+
+Bpoint = baseConect.cursor(buffered=True)
+
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #This is main of Emi
+
+import EmiVoiceProcess.voiceModule as vM
+
+#  V   V   O O   I I I   C C  E E E
+#   V V   O   O    I    C     E E
+#    V     O O   I I I   C C  E E E
+
+print(vM.engVoiceConfig())
+print(vM.micConfig())
+print(vM.voiceProcess())
